fca0a49a4994f643f846b81c3000e6baca696150
Player <player@player.to>
2022-05-28 22:35:22 +0200

    normalize gamedir

--- src/main/java/net/fabricmc/loader/impl/FabricLoaderImpl.java
+++ src/main/java/net/fabricmc/loader/impl/FabricLoaderImpl.java
@@ -141,7 +141,7 @@ public final class FabricLoaderImpl extends net.fabricmc.loader.FabricLoader {
 	}
 
 	private void setGameDir(Path gameDir) {
-		this.gameDir = gameDir;
+		this.gameDir = gameDir.toAbsolutePath().normalize();
 		this.configDir = gameDir.resolve("config");
 	}
 
